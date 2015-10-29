d7e27107f0752ce78792b7690fe18977a3eaf4c6
Yong Jae Kim <youthful2012@naver.com>
2015-10-29 23:04:05 +0900

    success the exercise

--- Udemy-Mutiples/ViewController.swift
+++ Udemy-Mutiples/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var logoImg: UIImageView!
+    @IBOutlet var inputField: UITextField!
+    @IBOutlet var playBtn: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var addBtn: UIButton!
+    
+    var resultNum: Int! = 0
+    var currentNum: Int! = 1
+    var firstNum: Int! = 0
+    
+    @IBAction func playButtonPressed(sender: AnyObject) {
+        
+        if (inputField.text != nil && inputField.text != ""){
+            
+            changeComponents()
+            currentNum = 1
+            firstNum = 0
+            resultNum = Int(inputField.text!)!
+            
+            resultLabel.text = "\(firstNum) + \(resultNum) = \(resultNum)"
+            firstNum = firstNum + resultNum
+            
+        } else {
+            
+            // UIAlertController Example
+            let alert = UIAlertController(title: "", message: "입력을 해주세요!", preferredStyle: .Alert)
+            
+            let alertAction = UIAlertAction(title: "확인", style: .Default, handler: nil)
+            alert.addAction(alertAction)
+            
+            self.presentViewController(alert, animated: false, completion: nil)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed(sender: AnyObject) {
+        
+        let maxNum = resultNum
+        if (currentNum < maxNum){
+            resultLabel.text = "\(firstNum) + \(maxNum) = \(firstNum + maxNum)"
+            firstNum = firstNum + maxNum
+        } else {
+            
+            resultLabel.text = ""
+            changeComponents()
+            
+        }
+        
+        currentNum!++
     }
-
-
+    
+    func changeComponents() {
+        logoImg.hidden = !logoImg.hidden
+        inputField.hidden = !inputField.hidden
+        playBtn.hidden = !playBtn.hidden
+        resultLabel.hidden = !resultLabel.hidden
+        addBtn.hidden = !addBtn.hidden
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
 }
 
